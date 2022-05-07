0338756705105e333ccc5a0eb334b4083fc571c3
Debleena18 <sarkardebleena18@gmail.com>
2022-05-07 17:17:28 +0530

    few changes

--- src/Row.js
+++ src/Row.js
@@ -1,5 +1,8 @@
 import React, {useState, useEffect} from 'react'
 import axios from 'axios';
+import "./Row.css";
+
+const base_url = "https://image.tmdb.org/t/p/original/";
 
 function Row( {title, fetchUrl}) {
     const [movies, setMovies] = useState([]);
@@ -7,6 +10,7 @@ function Row( {title, fetchUrl}) {
     useEffect(() => {
       async function fetchData() {
         const request = await axios.get(fetchUrl);
+        //console.log("Req", request)
         setMovies(request.data.results)
         return request;
       }
@@ -21,7 +25,10 @@ function Row( {title, fetchUrl}) {
 
         <div className='row__posters'>
           {movies.map(movie => (
-            <img src={movie.poster_path} alt={movie.name} />
+            <img 
+            key={movie.id}
+            className='row__poster'
+            src={`${base_url}${movie.poster_path}`} alt={movie.name} />
           ))}
 
         </div>
