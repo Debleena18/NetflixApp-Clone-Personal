931b49a189cd6777fe57d95e6c96635b92dc9a1b
Debleena18 <sarkardebleena18@gmail.com>
2022-05-07 17:27:48 +0530

    banner added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import './App.css';
-import Row from './Row';
+import Banner from './Components/Banner';
+import Navbar from './Components/Navbar';
+import Row from './Components/Row';
 import requests from './requests';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome To Netflix</h1>
-      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals}/>
-      <Row title="TRENDING NOW" fetchUrl={requests.fetchTrending} />
+    <div className="app">
+
+      <Navbar />
+      
+      <Banner />
+       
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} 
+      isLargeRow />
+
+      <Row title="Trending now" fetchUrl={requests.fetchTrending} />
+
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+
+     
     </div>
   );
 }
